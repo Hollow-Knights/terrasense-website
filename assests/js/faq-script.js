7cf96542793f0fd53d7a4e639277c7587cb9c329
Guilherme Schumann <gui.rsc45@gmail.com>
2024-11-01 07:54:49 -0300

    Construção da sessão FAQ

---
+++ assests/js/faq-script.js
@@ -0,0 +1,16 @@
+const buttons = document.querySelectorAll('.faq-btn');
+
+buttons.forEach(button => {
+  button.addEventListener('click', () => {
+    const description = button.closest('.question').querySelector('.faq-description');
+    description.classList.toggle('faq-description-off');
+
+    const img = button.querySelector('img');
+
+    if (description.classList.contains('faq-description-off')) {
+      img.src = './src/faq-arrow-close.svg';
+    } else {
+      img.src = './src/faq-arrow-open.svg';
+    };
+  });
+});
